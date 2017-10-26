2d6db040e779513b3e09c4acec61afd6cd24b719
Joysond <joyson.ullal@gmial.com>
2017-10-26 18:40:04 +0530

    initial commit

---
+++ src/main/java/com/vit/helloworldmvc/config/ApplicationContextConfig.java
@@ -0,0 +1,18 @@
+package com.vit.helloworldmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+ 
+@Configuration
+@ComponentScan("com.vit.helloworldmvc.*") 
+public class ApplicationContextConfig { 
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
